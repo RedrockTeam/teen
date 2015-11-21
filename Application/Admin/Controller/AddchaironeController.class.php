efc4e8808c967beb8bd84d3ae7f934323a1fbd74
duwonders <752046798@qq.com>
2015-11-21 17:02:25 +0800

    ....

--- Application/Admin/Controller/AddchaironeController.class.php
+++ Application/Admin/Controller/AddchaironeController.class.php
@@ -13,5 +13,18 @@ class AddchaironeController extends Controller {
 	public function index(){
 		$this->display();
 	} 
+	public function add(){
+		if(session('manager')){
+			$picture = '__PUBLIC__/chairone'.$_FILES['myfile']['name'];
+			move_uploaded_file($_FILES['myfile']['tmp_name'] , $picture);
+			$where = array(
+				'chairname' => I('get.username'),
+				'password' => I('get.password'),
+				'information' => I('get.info'),
+				'sex' => I('get.sex'),
+				'picture' => 
+			);
+		}
+	}
 
 }
