fedec87102111f1e4bdfed0d9fc34e06f6e09cd6
duwonders <752046798@qq.com>
2015-11-22 18:07:25 +0800

    dsa

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,7 +3,10 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-    	//$this->assgin('voice', $voice);          //这里缺少登陆状态和前端渲染的数据
+        $chairman = M('chairman')->field('id', 'chairname')->select();
+        $voice = $this->load_home_data();
+    	$this->assgin('voice', $voice);          //这里缺少登陆状态和前端渲染的数据
+        $this->assgin('chairman', $chairman);
         $this->display();
     }
 
@@ -82,9 +85,13 @@ class IndexController extends Controller {
 
     //首页加载的方法
     public function load_home_data(){
-        $id = I('get.id');                      //获取上一次查询的终点id
+        $id = I('get.id');                    //获取上一次查询的终点id
         $data = D('voice')->home_load_data($id);
-        $this->ajaxReturn($data, 'json');
+        if($id){
+            $this->ajaxReturn($data, 'json');
+        }else{
+            return $data;
+        }
     }
 
     /*提问的方法
