1c85e31fb81b93c85a3be2f575a58fba947ac82f
Excelsior-WYH <1419290661@qq.com>
2015-11-22 20:06:39 +0800

    末班渲染

--- Application/Mobile/Controller/IndexController.class.php
+++ Application/Mobile/Controller/IndexController.class.php
@@ -7,8 +7,8 @@
 	    public function index(){
 	    	
 	    	$chairman = M('chairman')->field('id, chairname')->select();
-	    	var_dump($chairman);
-	        // $voice = R('Home/load_home_data');
+	        $voice = R('Home/load_home_data');
+	        var_dump($voice);
 	    	// $this->assign('voice', $voice);          //这里缺少登陆状态和前端渲染的数据
 	        // $this->assign('chairman', $chairman);
 	    	$this->display();
