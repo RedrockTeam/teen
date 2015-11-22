afeb97e2c5f1f166222a08f21aeb69b2040bb52f
Excelsior-WYH <1419290661@qq.com>
2015-11-22 20:25:26 +0800

    末班渲染

--- Application/Mobile/Controller/IndexController.class.php
+++ Application/Mobile/Controller/IndexController.class.php
@@ -5,8 +5,22 @@
 	    
 
 	    public function index(){
+	    	$chairman = M('chairman')->field('id, chairname')->select();
+	        $voice = $this->load_home_data();
+	    	$this->assgin('voice', $voice);          //这里缺少登陆状态和前端渲染的数据
+	        $this->assgin('chairman', $chairman);
 	    	$this->display();
+	    }
 
+	    //首页加载的方法
+	    public function load_home_data(){
+	        $id = I('get.id');                    //获取上一次查询的终点id
+	        $data = D('voice')->home_load_data($id);
+	        if($id){
+	            $this->ajaxReturn($data, 'json');
+	        }else{
+	            return $data;
+	        }
 	    }
 
 	}
