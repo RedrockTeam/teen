e9a259b175da039bc8a6f337d7555a11f55eff84
Excelsior-WYH <1419290661@qq.com>
2015-11-22 20:08:58 +0800

    末班渲染

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,7 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        $chairman = M('chairman')->field('id', 'chairname')->select();
+        $chairman = M('chairman')->field('id, chairname')->select();
         $voice = $this->load_home_data();
     	$this->assgin('voice', $voice);          //这里缺少登陆状态和前端渲染的数据
         $this->assgin('chairman', $chairman);
