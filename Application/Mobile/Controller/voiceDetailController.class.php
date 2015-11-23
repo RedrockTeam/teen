d46a4e3f7a58dcd23ba2dc0d6817f86e9e2f0e0e
Excelsior-WYH <1419290661@qq.com>
2015-11-23 22:26:26 +0800

    问题详情页

--- Application/Mobile/Controller/voiceDetailController.class.php
+++ Application/Mobile/Controller/voiceDetailController.class.php
@@ -3,7 +3,7 @@ namespace Mobile\Controller;
 use Think\Controller;
 class VoiceDtailController extends Controller {
 	public function _before_index(){
-		if($session('username')){
+		if(session('username')){
 			$this->error('请先登陆！');
 		}
 	}
