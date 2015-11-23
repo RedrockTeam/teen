1c3662ddece9f31d0f51f491c8e129c1e8d7ea92
Excelsior-WYH <1419290661@qq.com>
2015-11-23 22:29:13 +0800

    问题详情页

--- Application/Mobile/Controller/IndexController.class.php
+++ Application/Mobile/Controller/IndexController.class.php
@@ -13,6 +13,45 @@
 	    	$this->display();
 	    }
 
+	    public function detail() {
+	    	$id = I('get.id');
+			$data = $this->get_voice_detail($id);
+			$this->assign('info', $data);
+			print_r($data);
+			$this->display('detail');
+	    }
+
+	    private function get_voice_detail($id){
+			if(!$id){
+				$data = array(
+					'status' => 300,
+					'message' => '无效id'
+				);
+				$this->ajaxReturn($data, 'json');
+			}else{
+				$where = array(
+					'voice.id' => $id,
+				);
+				$commentData = M('voice')->field('comment.time, comment.username, comment.comment, comment.userid')->join("comment ON voice.id = comment.voiceid")->where($where)->select();
+				$voiceData = M('voice')->where('id='.$id)->select();
+				foreach ($commentData as $key => $value) {
+					if($value['userid'] == 'chairman'){
+						$commentData[$key]['image'] = M('chairman')->field('picture')->where('chairname='.$value['username'])->select();
+					}else{
+						$commentData[$key]['image'] = 'public/chairone/man.jpg';
+					}
+				}
+				$data['comment'] = $commentData;
+				$data['voice'] = $voiceData[0];
+				if($voiceData['posterid'] == 'chairman'){ 		//把主席号改成chairman
+					$data['voice']['image'] = M('chairman')->field('picture')->where($where)->select();
+				}else{
+					$data['voice']['image'] = 'public/chairone/man.jpg';
+				}
+				return $data;
+			}
+		}
+
 	    //首页加载的方法
 	    public function load_home_data(){
 	        $id = I('get.id');                    //获取上一次查询的终点id
--- Application/Mobile/Controller/voiceDetailController.class.php
+++ Application/Mobile/Controller/voiceDetailController.class.php
@@ -8,43 +8,8 @@ class VoiceDtailController extends Controller {
 		}
 	}
 	public function index(){
-		$id = I('get.id');
-		echo $id;
-		return;
-		$data = $this->get_voice_detail($id);
-		$this->assign('info', $data);
-		print_r($data);
-		$this->display('Index/detail');
+		
 	}
 
-	private function get_voice_detail($id){
-		if(!$id){
-			$data = array(
-				'status' => 300,
-				'message' => '无效id'
-			);
-			$this->ajaxReturn($data, 'json');
-		}else{
-			$where = array(
-				'voice.id' => $id,
-			);
-			$commentData = M('voice')->field('comment.time, comment.username, comment.comment, comment.userid')->join("comment ON voice.id = comment.voiceid")->where($where)->select();
-			$voiceData = M('voice')->where('id='.$id)->select();
-			foreach ($commentData as $key => $value) {
-				if($value['userid'] == 'chairman'){
-					$commentData[$key]['image'] = M('chairman')->field('picture')->where('chairname='.$value['username'])->select();
-				}else{
-					$commentData[$key]['image'] = 'public/chairone/man.jpg';
-				}
-			}
-			$data['comment'] = $commentData;
-			$data['voice'] = $voiceData[0];
-			if($voiceData['posterid'] == 'chairman'){ 		//把主席号改成chairman
-				$data['voice']['image'] = M('chairman')->field('picture')->where($where)->select();
-			}else{
-				$data['voice']['image'] = 'public/chairone/man.jpg';
-			}
-			return $data;
-		}
-	}
+	
 }
