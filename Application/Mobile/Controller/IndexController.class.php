88447a1fdfca81223cd2e87d07f915e3ada9bb8b
Excelsior-WYH <1419290661@qq.com>
2015-11-24 21:41:48 +0800

    更换首页图

--- Application/Mobile/Controller/IndexController.class.php
+++ Application/Mobile/Controller/IndexController.class.php
@@ -5,14 +5,16 @@
 	    
 
 	    public function index(){
-	    	
 	    	$this->display();
-
 	    }
 
 	    public function detail() {
 	    	$voiceId = I('get.id');
 	    	$this->assign('voiceId', $voiceId);
+	    	$this->display();
+	    }
+
+	    public function personal() {
 	    	$this->display();
 	    }
 	}
