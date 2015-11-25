09c113506b729e18c0179c829a1fde4d5f799723
Excelsior-WYH <1419290661@qq.com>
2015-11-25 19:32:33 +0800

    无评论哭脸显示

--- Application/Mobile/Controller/IndexController.class.php
+++ Application/Mobile/Controller/IndexController.class.php
@@ -33,19 +33,41 @@
 				$commentData = M('voice')->field('comment.time, comment.username, comment.comment, comment.userid')->join("comment ON voice.id = comment.voiceid")->where($where)->select();
 				$voiceData = M('voice')->where('id='.$id)->select();
 				foreach ($commentData as $key => $value) {
-					if($value['userid'] == 'chairman'){
-						$commentData[$key]['image'] = M('chairman')->field('picture')->where('chairname='.$value['username'])->select();
+					if(strlen($value['userid']) == 5){
+						$where = array(
+							'id' => $value['userid']
+						);
+						$commentData[$key]['image'] = M('chairman')->field('picture')->where($where)->select()[0]['picture'];
 					}else{
 						$commentData[$key]['image'] = 'public/chairone/man.jpg';
 					}
 				}
+
+				//分别获取评论和问题
 				$data['comment'] = $commentData;
 				$data['voice'] = $voiceData[0];
-				if($voiceData['posterid'] == 'chairman'){ 		//把主席号改成chairman
-					$data['voice']['image'] = M('chairman')->field('picture')->where($where)->select();
+				//将结果负值给data数组
+
+				if(strlen($voiceData[0]['posterid']) == 5){ 
+					$where = array(
+						'id' => $voiceData[0]['posterid']
+					);
+					$data['voice']['image'] = M('chairman')->field('picture')->where($where)->select()[0]['picture'];
 				}else{
 					$data['voice']['image'] = 'public/chairone/man.jpg';
 				}
+				//将照片地址添加到结果数组里
+				$where = array(
+					'voiceid' => $id,
+					'user' => session('stunum'),
+				);
+				if(M('user_vote')->where($where)->select()){
+					$data['is_voted'] = true;
+				}else{
+					$data['is_voted'] = false;
+				}
+				//查看是否已经对该问题点过赞
+				dump($data);
 				return $data;
 			}
 		}
