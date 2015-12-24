377d3535d77e4ce28287896ffdc75efc50b39a28
Excelsior-WYH <1419290661@qq.com>
2015-12-24 17:43:52 +0800

    bug

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -4,6 +4,49 @@ use Think\Controller;
 class IndexController extends Controller {
     
     public function index(){
+        $weiboArray = array(
+            array(
+                "name" =>"重邮青年",
+                "img" => "/teen/Public/home/images/weibo/teen.jpg",
+                "url" => "http://weibo.com/u/2361881682?topnav=1&wvr=6&topsug=1&is_all=1",
+            ),
+            array(
+                "name" =>"重邮官微",
+                "img" => "/teen/Public/home/images/weibo/you.jpg",
+                "url" => "http://weibo.com/cqupt1950?from=page_100206_profile&wvr=6&mod=bothfollow&is_all=1",
+            ),
+            array(
+                "name" =>"重邮科联",
+                "img" => "/teen/Public/home/images/weibo/sci.jpg",
+                "url" => "http://weibo.com/cqxskjlhh?is_hot=1",
+            ),
+            array(
+                "name" =>"学生会",
+                "img" => "/teen/Public/home/images/weibo/hui.jpg",
+                "url" => "http://weibo.com/cquptxsh?is_hot=1",
+            ),
+            array(
+                "name" =>"重邮社联",
+                "img" => "/teen/Public/home/images/weibo/soc.jpg",
+                "url" => "http://weibo.com/cquptsau?is_hot=1",
+            ),
+            array(
+                "name" =>"重邮团委",
+                "img" => "/teen/Public/home/images/weibo/tuan.jpg",
+                "url" => "http://weibo.com/tuanweicqupt?is_hot=1",
+            ),
+            array(
+                "name" =>"大艺团",
+                "img" => "/teen/Public/home/images/weibo/art.jpg",
+                "url" => "http://weibo.com/u/3219843654?is_hot=1",
+            ),
+            array(
+                "name" => "志愿者",
+                "img" => "/teen/Public/home/images/weibo/vol.jpg",
+                "url" => "http://weibo.com/cquptqnzyz?is_hot=1"
+            )
+        );
+        $this->assign('weibo', $weiboArray);
         $this->flash();
         $this->display();
     }
